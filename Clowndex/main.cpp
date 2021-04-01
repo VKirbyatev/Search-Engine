faa820f24176cda514e7636cf6f357b944c4d592
Vladislav Kirbiatev <vkitbyatev@mail.ru>
2021-04-01 21:17:56 +0300

    Создал .doc файлы для поиска по ним, а так же validation файл для контроля

--- Clowndex/main.cpp
+++ Clowndex/main.cpp
@@ -1,16 +1,8 @@
-//
-//  main.cpp
-//  Clowndex
-//
-//  Created by Владислав on 01.04.2021.
-//  Copyright © 2021 Vladislav Kirbyatev. All rights reserved.
-//
-
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    //cringe asd
+    
+    
+
     return 0;
 }
