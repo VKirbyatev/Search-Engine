f916cfe14a57ca86b4732cee7ce0fa4aa70e2566
Vladislav Kirbiatev <vkitbyatev@mail.ru>
2021-04-01 20:56:50 +0300

    Initial Commit

---
+++ Clowndex/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Clowndex
+//
+//  Created by Владислав on 01.04.2021.
+//  Copyright © 2021 Vladislav Kirbyatev. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
